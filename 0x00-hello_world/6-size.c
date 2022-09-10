9f0650c8153c5b8a941aa170582296ace5410d74
muzameil <muzh2004@gmail.com>
2022-09-10 05:41:39 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,19 +4,18 @@
 *
 *Description: using the main function
 *return: 0
-*/
-int main(void)
+*/int main(void)
+
 {
 char c;
 int i;
 long li;
 long long lli;
 float f;
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(i));
+printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
+return (0);
 
-printf("Size of a char:%ld byte(s)\n", sizeof(c));
-printf("Size of a an int:%ld byte(s)\n", sizeof(i));
-printf("Size of a long int:%ld byte(s)\n", sizeof(li));
-printf("Size of a long long int:%ld byte(s)\n", sizeof(lli));
-printf("Size of a float :%ld byte(s)\n", sizeof(f));
-return(0);
-}
