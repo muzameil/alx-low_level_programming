1bee8995f7eeb1d547353d5b8b28140a316ebb10
muzameil <muzh2004@gmail.com>
2022-09-10 07:08:25 -0700

    tsk 8

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*
+* main - print the string in the put function
+*
+* Description: using the main function
+*
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+in main(void)
+{
+char ch;
+for (ch='a';ch<'z';ch++)
+{
+	putchar(ch);
+}
+putchar('\n'');
+return (0);
+}
