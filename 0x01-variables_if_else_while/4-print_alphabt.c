3dc12053a4d19e726121c735bd9f48728ed8bd2c
muzameil <muzh2004@gmail.com>
2022-09-12 08:43:25 -0700

    task5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - print if the number is postive, zero, or negative
 * Description: using the main function
@@ -8,22 +7,15 @@
 */
 
 int main(void)
-
 {
-
-	char ch;
-
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
 if (ch != 'e' && ch != 'q')
 {
 putchar(ch);
 }
 }
-
-	putchar('\n');
-
-	return (0);
-
+putchar('\n');
+return (0);
 }
